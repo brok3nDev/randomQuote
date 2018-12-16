33a9f7020f7701d54d0ad2dcc0ebb41938a6efd2
firstfloor <firstfloor@brok3nDev-2.local>
2018-12-16 09:21:22 -0600

    fifth commit

--- js/script.js
+++ js/script.js
@@ -1,94 +1,73 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 1 - A Random Quote Generator
-******************************************/
-
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-
-  Recommended: 
-    - Add at least one `year` and/or `citation` property to at least one 
-      quote object.
-***/
-
-var message = '';
-var loadQuote;
-var randomQuote;
+var bgColors = [
+  'Gold',
+  'Blue',
+  'Silver',
+  'Teal',
+  'Orange'
+  ];
 
 let quotes = [
-  {quote: “Defining myself is one of the most difficult challenges I face.”, 
+  {quote: "Defining myself is one of the most difficult challenges I face.",
    source: "Carol Moseley-Braun",
   },
-  {quote: “Hate is too great a burden to bear. It injures the hater more than it injures the hated.”, 
+  {quote: "Hate is too great a burden to bear. It injures the hater more than it injures the hated.", 
    source: "Coretta Scott King",
   },
-  {quote: “In recognizing the humanity of our fellow beings we pay ourselves the highest tribute.”, 
-   source: "Thurgood Marshall" 
+  {quote: "In recognizing the humanity of our fellow beings we pay ourselves the highest tribute.",
+   source: "Thurgood Marshall",
   },
-  {quote: “Whatever we believe about ourselves and our ability comes true for us.”, 
-   source: "Susan L. Taylor" 
+  {quote: "Whatever we believe about ourselves and our ability comes true for us.",
+   source: "Susan L. Taylor",
   },
-  {quote: “Imagination is more important than knowledge.”, 
-   source: "Albert Einstein" 
+  {quote: "Imagination is more important than knowledge.", 
+   source: "Albert Einstein" ,
   }
 ];
 
 
 
-/***
-  Create the `getRandomQuote` function to:
-   - generate a random number 
-   - use the random number to `return` a random quote object from the 
-     `quotes` array.
-***/
+//Created random generator 
 
-
-function print(quote) {
-	var outputDiv = document.getElementById('quote-box');
-	outputDiv.innerHTML = quote;
-}
-
-function getRandomQuote () {
+function getRandomQuote (array) {
    var randomQuote = Math.floor(Math.random() * quotes.length);
-   let myRandomNumber = Math.random();
-   return Math.floor(myRandomNumber)
-}
+   return quotes[randomQuote];
+};
 
 getRandomQuote(loadQuote);
 
-/***
-  Create the `printQuote` function to: 
-   - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to 
-     create your HTML string.
-   - use conditionals to make sure the optional properties exist before 
-     they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
 
-function printQuote(message) {
-  var printQuote = getRandomQuote(quotes[randomQuote]);
-  var HTML="";
-  HTML += "<p class='quote'>" + result.quote + "</p>" +
-    "<p class='source'>" + result.source + "</p>";
-  document.getElementById('quote-box').innerHTML = HTML;
-  return HTML;
+//print function 
+
+function printQuote() {
+  var getQuote = getRandomQuote(quotes);
+  var stringOfQuoteProperties = '<p class="quote">' + getQuote.quote + '</p>';
+  stringOfQuoteProperties += '<p class="source">' + getQuote.source;
+
+
+stringOfQuoteProperties += '<span class="tag">' + getQuote.tag + '</span> </p>';
+
+document.getElementById('quote-box').innerHTML = stringOfQuoteProperties;
+};
+
+printQuote();
+
+
+
+function getRandomColor(array) {
+  var randomColor = Math.floor(Math.random() * bgColors.length);
+  return bgColors[randomColor];
+};
+
+// bgcolor change
+function setBackgroundColor() {
+  var generateColor = getRandomColor(bgColors);
+  document.querySelector("body").style.background = generateColor;
 }
 
+setBackgroundColor();
 
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+document.getElementById('loadQuote').addEventListener("click", setBackgroundColor, false);
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
